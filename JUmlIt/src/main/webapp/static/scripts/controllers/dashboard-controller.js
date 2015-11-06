2c302fc79123a7fca84c99d31ae7fe82c22e2951
Maxim <caliber1997@gmail.com>
2015-11-06 02:04:47 +0200

    Edited styles of navbar, added modal markup, added modal styles

--- JUmlIt/src/main/webapp/static/scripts/controllers/dashboard-controller.js
+++ JUmlIt/src/main/webapp/static/scripts/controllers/dashboard-controller.js
@@ -2,7 +2,8 @@
 angular.module('jumlitApp').controller('DashboardCtrl', function($scope, $uibModal) {
     $scope.editDetails = function() {
         $uibModal.open({
-            
+            templateUrl: 'modals/personal-details-modal.html',
+            controller: 'PersonalDetailsModalCtrl'
         });
     };
 });
--- JUmlIt/src/main/webapp/static/scripts/controllers/personal-details-modal-controller.js
+++ JUmlIt/src/main/webapp/static/scripts/controllers/personal-details-modal-controller.js
@@ -1,4 +1,12 @@
 'use strict';
 angular.module('jumlitApp').controller('PersonalDetailsModalCtrl', function ($scope, $uibModalInstance) {
+    $scope.personal = '';
 
+    $scope.save = function () {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss();
+    };
 });
