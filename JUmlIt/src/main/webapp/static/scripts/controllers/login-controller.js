3b20f90657e470151797f32a3be10decb2ddedb3
Max <max.selekh@gmail.com>
2015-11-06 16:40:11 +0200

    login

--- JUmlIt/src/main/webapp/static/scripts/controllers/login-controller.js
+++ JUmlIt/src/main/webapp/static/scripts/controllers/login-controller.js
@@ -1,5 +1,47 @@
 'use strict';
 
 angular.module('jumlitApp').controller('LoginCtrl', function () {
+ $scope.user = {
+        email: '',
+        password: '',
+    };
+
+    $scope.alerts = [];
+
+    $scope.attemptLogin = function() {
+        var data = {
+            email: $scope.user.email,
+            password: $scope.user.password
+        };
+
+        $scope.alerts = [];
+
+        submitLogin(data)
+            .then(function() {
+                $state.go('account.dashboard');
+            })
+            .catch(function (error) {
+                $scope.alerts.push({
+                    type: 'danger',
+                    msg: 'Login error! Something happened on our servers.'
+                });
+            });
+    }
+
+    function submitLogin(data) {
+        return Auth.login(data)
+            .catch(function(error) {
+                // TODO: handle error
+                $scope.alerts.push({
+                    type: 'danger',
+                    msg: 'Login error! Something happened on our servers.'
+                });
+                return $q.reject();
+            });
+    }
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
 
 });
