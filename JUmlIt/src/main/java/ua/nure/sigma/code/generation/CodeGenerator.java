044f97f5034617e1db95e8ea17921a1b91f12a2c
didevgen <didevgen@gmail.com>
2015-11-12 01:18:59 +0200

    Source code generation methods are ready

--- JUmlIt/src/main/java/ua/nure/sigma/code/generation/CodeGenerator.java
+++ JUmlIt/src/main/java/ua/nure/sigma/code/generation/CodeGenerator.java
@@ -1,40 +1,105 @@
 package ua.nure.sigma.code.generation;
 
-import java.io.IOException;
-
 import javax.lang.model.element.Modifier;
 
-import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.TypeSpec.Builder;
 
 import ua.nure.sigma.code.model.Clazz;
 import ua.nure.sigma.code.model.Field;
 import ua.nure.sigma.code.model.Interface;
 import ua.nure.sigma.code.model.Method;
+import ua.nure.sigma.code.model.MethodArg;
 
 public class CodeGenerator {
-	
+
 	public static final String MAIN_PACKAGE = "ua.nure.jumlit";
+
 	private GeneratorService service = new GeneratorService();
 
 	public static void main(String[] args) {
 	}
-	
-	private void generateMethod(Method method) {
-		
+
+	public TypeSpec generateClass(Clazz clazz) {
+		Builder builder = TypeSpec.classBuilder(clazz.getName());
+		for (String modifierName : clazz.getModifiers()) {
+			builder.addModifiers(service.getModifier(modifierName));
+		}
+		for (Interface iFace : clazz.getInterfaces()) {
+			ClassName interfaceName = ClassName.get(MAIN_PACKAGE, iFace.getName());
+			builder.addSuperinterface(interfaceName);
+		}
+		for (Field field : clazz.getFields()) {
+			builder.addField(generateField(field));
+		}
+		for (Method method : clazz.getMethods()) {
+			builder.addMethod(generateMethod(method));
+		}
+		return builder.build();
+	}
+
+	public TypeSpec generateInterface(Interface iFace) {
+		Builder interfaceBuilder = TypeSpec.interfaceBuilder(iFace.getName())
+			    .addModifiers(Modifier.PUBLIC);
+		for (Field field : iFace.getFields()) {
+			interfaceBuilder.addField(generateInterfaceField(field));
+		}
+		for (Method method : iFace.getMethods()) {
+			interfaceBuilder.addMethod(MethodSpec.methodBuilder(method.getName())
+			        .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+			        .build());
+		}
+		return interfaceBuilder.build();
+	}
+
+	private FieldSpec generateField(Field field) {
+		try {
+			com.squareup.javapoet.FieldSpec.Builder builder = FieldSpec.builder(
+					service.getTypeName(field.getType().getTypeName()), field.getName());
+			for (int i = 0; i < field.getModifiers().size(); i++) {
+				builder.addModifiers(service.getModifier(field.getModifiers().get(i)));
+			}
+			return builder.build();
+		} catch (Exception ex) {
+			return FieldSpec.builder(TypeName.BOOLEAN, "ERROR", Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).build();
+		}
 	}
 	
-	private void generateClass(Clazz clazz) {
-		
+	private FieldSpec generateInterfaceField(Field field) {
+		try {
+			com.squareup.javapoet.FieldSpec.Builder builder = FieldSpec.builder(
+					service.getTypeName(field.getType().getTypeName()), field.getName().toUpperCase(),Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL);
+			
+			return builder.build();
+		} catch (Exception ex) {
+			return FieldSpec.builder(TypeName.BOOLEAN, "ERROR", Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL).build();
+		}
+	}
+
+	private MethodSpec generateMethod(Method method) {
+		com.squareup.javapoet.MethodSpec.Builder builder = MethodSpec.methodBuilder(method.getName());
+		for (String modif :method.getModifiers()) {
+			builder.addModifiers(service.getModifier(modif));
+		}
+		for (MethodArg arg : method.getArguments()) {
+			builder.addParameter(prepareParam(arg));
+		}
+		builder.returns(service.getTypeName(method.getReturnType().getTypeName()));
+		return builder.build();
 	}
 	
-	private void generateField(Field field) {
-		
-	}
-	
-	private void generateInterface(Interface iFace) {
-		
+	private ParameterSpec prepareParam(MethodArg arg) {
+		com.squareup.javapoet.ParameterSpec.Builder builder = ParameterSpec.builder(service.
+				getTypeName(arg.getType().getTypeName()), arg.getName());
+		for (String modif : arg.getModifiers()) {
+			builder.addModifiers(service.getModifier(modif));
+		}
+		return builder.build();
 	}
 
 }
--- JUmlIt/src/main/java/ua/nure/sigma/code/generation/GeneratorService.java
+++ JUmlIt/src/main/java/ua/nure/sigma/code/generation/GeneratorService.java
@@ -2,8 +2,53 @@ package ua.nure.sigma.code.generation;
 
 import javax.lang.model.element.Modifier;
 
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.TypeName;
+
 public class GeneratorService {
 	public Modifier getModifier(String modifier) {
 		return Modifier.valueOf(modifier.toUpperCase());
+	}
+	
+	public TypeName getTypeName(String type) {
+		String currType = type.toLowerCase();
+		if (currType.equals("byte")) {
+			return TypeName.BYTE;
+		}
+		else if (currType.equals("short")) {
+			return TypeName.SHORT;
+		}
+		else if (currType.equals("int")) {
+			return TypeName.INT;
+		}
+		else if (currType.equals("long")) {
+			return TypeName.LONG;
+		}
+		else if (currType.equals("char")) {
+			return TypeName.CHAR;
+		}
+		else if (currType.equals("float")) {
+			return TypeName.FLOAT;
+		}
+		else if (currType.equals("double")) {
+			return TypeName.DOUBLE;
+		}
+		else if (currType.equals("boolean")) {
+			return TypeName.BOOLEAN;
+		}
+		else if (currType.equals("void")) {
+			return TypeName.VOID;
+		}
+		else if (currType.equals("object")) {
+			return TypeName.OBJECT;
+		}
+		else if (currType.equals("string")) {
+			ClassName interfaceName = ClassName.get("", "String");
+			return interfaceName;
+		}
+		else {
+			ClassName interfaceName = ClassName.get("", type);
+			return interfaceName;
+		}
 	}
 }
