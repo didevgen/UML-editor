2dfd490507258d4bdeb010908f942f5f00e4c50b
Maxim <caliber1997@gmail.com>
2015-11-27 21:50:47 +0200

    Updated db script

--- JUmlIt/src/main/webapp/resources/jumlit.sql
+++ JUmlIt/src/main/webapp/resources/jumlit.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 27, 2015 at 03:30 PM
--- Server version: 5.6.27-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.14
+-- Хост: 127.0.0.1
+-- Время создания: Ноя 27 2015 г., 20:39
+-- Версия сервера: 5.6.26
+-- Версия PHP: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,186 +14,278 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `jumlit`
+-- База данных: `jumlit`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `collaborator`
+-- Структура таблицы `collaborator`
 --
 
 CREATE TABLE IF NOT EXISTS `collaborator` (
   `user_id` bigint(20) DEFAULT NULL,
-  `diagram_id` bigint(20) DEFAULT NULL,
-  KEY `FK_Reference_2` (`user_id`),
-  KEY `FK_Reference_3` (`diagram_id`)
+  `diagram_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `diagram`
+-- Структура таблицы `diagram`
 --
 
 CREATE TABLE IF NOT EXISTS `diagram` (
-  `diagram_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `diagram_id` bigint(20) NOT NULL,
   `owner_id` bigint(20) DEFAULT NULL,
   `status_id` bigint(20) DEFAULT NULL,
   `json_data` varchar(2056) COLLATE utf8_general_mysql500_ci NOT NULL,
   `created_date` datetime NOT NULL,
   `last_updated` datetime NOT NULL,
   `name` varchar(256) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
-  `description` varchar(3000) COLLATE utf8_general_mysql500_ci DEFAULT NULL,
-  PRIMARY KEY (`diagram_id`),
-  KEY `FK_Reference_1` (`owner_id`),
-  KEY `FK_Reference_4` (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `diagram_status`
---
-
-CREATE TABLE IF NOT EXISTS `diagram_status` (
-  `status_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `status_type` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL,
-  PRIMARY KEY (`status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `event`
---
-
-CREATE TABLE IF NOT EXISTS `event` (
-  `event_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `event_type_id` bigint(20) DEFAULT NULL,
-  `timestamp` datetime NOT NULL,
-  PRIMARY KEY (`event_id`),
-  KEY `FK_Reference_5` (`event_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `eventtype`
---
-
-CREATE TABLE IF NOT EXISTS `eventtype` (
-  `event_type_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL,
-  PRIMARY KEY (`event_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `history`
---
-
-CREATE TABLE IF NOT EXISTS `history` (
-  `event_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  KEY `FK_Reference_6` (`event_id`),
-  KEY `FK_Reference_7` (`user_id`)
+  `description` varchar(3000) COLLATE utf8_general_mysql500_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `token`
+-- Структура таблицы `diagram_status`
 --
 
-CREATE TABLE IF NOT EXISTS `token` (
-  `tokenId` int(11) NOT NULL AUTO_INCREMENT,
-  `tokenValue` varchar(45) DEFAULT NULL,
-  `userId` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`tokenId`),
-  KEY `forket_idx` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+CREATE TABLE IF NOT EXISTS `diagram_status` (
+  `status_id` bigint(20) NOT NULL,
+  `status_type` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Структура таблицы `event`
+--
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `event_id` bigint(20) NOT NULL,
+  `event_type_id` bigint(20) DEFAULT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `eventtype`
+--
+
+CREATE TABLE IF NOT EXISTS `eventtype` (
+  `event_type_id` bigint(20) NOT NULL,
+  `description` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `history`
+--
+
+CREATE TABLE IF NOT EXISTS `history` (
+  `event_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `token`
+--
+
+CREATE TABLE IF NOT EXISTS `token` (
+  `tokenId` int(11) NOT NULL,
+  `tokenValue` varchar(45) DEFAULT NULL,
+  `userId` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `token`
+--
+
+INSERT INTO `token` (`tokenId`, `tokenValue`, `userId`) VALUES
+(3, 'ebeb300882677f350ea818c8f333f5b9', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
   `full_name` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL,
   `email` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL,
   `password` varchar(256) COLLATE utf8_general_mysql500_ci NOT NULL,
   `registration_date` datetime NOT NULL,
-  `last_available` datetime NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci AUTO_INCREMENT=6 ;
+  `last_available` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 
 --
--- Dumping data for table `user`
+-- Дамп данных таблицы `user`
 --
 
 INSERT INTO `user` (`user_id`, `full_name`, `email`, `password`, `registration_date`, `last_available`) VALUES
-(1, 'Maxim Semikin', 'maxim.semikin@gmail.com', '$2a$10$VeR2bSE78J60m.wG8kcE5.LaAqx2Tb/Gp/SFA3s1u114RBshoVyFK', '2015-11-27 15:28:45', '2015-11-27 15:28:45');
+(1, 'Евгений Ковалев', 'didevgen@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2015-11-13 12:36:22', '2015-11-13 12:36:22'),
+(2, 'jdhsjfhs', 'selekh@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2015-11-20 17:11:05', '2015-11-20 17:11:05');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_role`
+-- Структура таблицы `user_role`
 --
 
 CREATE TABLE IF NOT EXISTS `user_role` (
   `role_id` bigint(20) NOT NULL,
   `role` varchar(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `user_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user_role`
+-- Дамп данных таблицы `user_role`
 --
 
 INSERT INTO `user_role` (`role_id`, `role`, `user_id`) VALUES
-(1, 'ROLE_USER', 1);
+(1, 'ROLE_USER', 1),
+(2, 'ROLE_USER', 2);
 
 --
--- Constraints for dumped tables
+-- Индексы сохранённых таблиц
 --
 
 --
--- Constraints for table `collaborator`
+-- Индексы таблицы `collaborator`
+--
+ALTER TABLE `collaborator`
+  ADD KEY `FK_Reference_2` (`user_id`),
+  ADD KEY `FK_Reference_3` (`diagram_id`);
+
+--
+-- Индексы таблицы `diagram`
+--
+ALTER TABLE `diagram`
+  ADD PRIMARY KEY (`diagram_id`),
+  ADD KEY `FK_Reference_1` (`owner_id`),
+  ADD KEY `FK_Reference_4` (`status_id`);
+
+--
+-- Индексы таблицы `diagram_status`
+--
+ALTER TABLE `diagram_status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Индексы таблицы `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `FK_Reference_5` (`event_type_id`);
+
+--
+-- Индексы таблицы `eventtype`
+--
+ALTER TABLE `eventtype`
+  ADD PRIMARY KEY (`event_type_id`);
+
+--
+-- Индексы таблицы `history`
+--
+ALTER TABLE `history`
+  ADD KEY `FK_Reference_6` (`event_id`),
+  ADD KEY `FK_Reference_7` (`user_id`);
+
+--
+-- Индексы таблицы `token`
+--
+ALTER TABLE `token`
+  ADD PRIMARY KEY (`tokenId`),
+  ADD KEY `forket_idx` (`userId`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Индексы таблицы `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `diagram`
+--
+ALTER TABLE `diagram`
+  MODIFY `diagram_id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `diagram_status`
+--
+ALTER TABLE `diagram_status`
+  MODIFY `status_id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `eventtype`
+--
+ALTER TABLE `eventtype`
+  MODIFY `event_type_id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `token`
+--
+ALTER TABLE `token`
+  MODIFY `tokenId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `collaborator`
 --
 ALTER TABLE `collaborator`
   ADD CONSTRAINT `FK_Reference_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_Reference_3` FOREIGN KEY (`diagram_id`) REFERENCES `diagram` (`diagram_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `diagram`
+-- Ограничения внешнего ключа таблицы `diagram`
 --
 ALTER TABLE `diagram`
   ADD CONSTRAINT `FK_Reference_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `FK_Reference_4` FOREIGN KEY (`status_id`) REFERENCES `diagram_status` (`status_id`);
 
 --
--- Constraints for table `event`
+-- Ограничения внешнего ключа таблицы `event`
 --
 ALTER TABLE `event`
   ADD CONSTRAINT `FK_Reference_5` FOREIGN KEY (`event_type_id`) REFERENCES `eventtype` (`event_type_id`);
 
 --
--- Constraints for table `history`
+-- Ограничения внешнего ключа таблицы `history`
 --
 ALTER TABLE `history`
   ADD CONSTRAINT `FK_Reference_6` FOREIGN KEY (`event_id`) REFERENCES `event` (`event_id`),
   ADD CONSTRAINT `FK_Reference_7` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
--- Constraints for table `token`
+-- Ограничения внешнего ключа таблицы `token`
 --
 ALTER TABLE `token`
   ADD CONSTRAINT `forket` FOREIGN KEY (`userId`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
